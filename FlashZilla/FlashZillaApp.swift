9f8f95f3fe995874869e8ea44e6ca8591b6cbee1
Гамид Халидов <khalidov-gamid@mail.ru>
2024-07-01 14:05:24 +0300

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Gamıd Khalıdov on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
