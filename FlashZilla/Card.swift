95e2a0163403d5d87b2e8a97a2e97fcd2f09b9c5
Гамид Халидов <khalidov-gamid@mail.ru>
2024-07-02 15:50:59 +0300

    Работают анимации, перезапуск игры, Таймер.

---
+++ FlashZilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Gamıd Khalıdov on 02.07.2024.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -7,15 +7,95 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset*10)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    @State var timeRemaining = 10
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State var isActive = true
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            VStack{
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background{
+                        Rectangle()
+                            .fill(.black)
+                            .opacity(0.4)
+                            .clipShape(.capsule)
+                    }
+                if cards.isEmpty || timeRemaining == 0 {
+                    Button{
+                        resetCards()
+                    } label: {
+                        Text("Restart")
+                            .font(.largeTitle)
+                            .padding()
+                            .background{
+                                Rectangle()
+                                    .fill(.black)
+                                    .opacity(0.4)
+                                    .clipShape(.capsule)
+                            }
+                    }
+                }
+                
+                ZStack{
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                   removeCard(at: index)
+                               }
+                        }
+                            .stacked(at: index, in: cards.count)
+                            .allowsHitTesting(!(timeRemaining == 0))
+                    }
+                }
+            }
         }
-        .padding()
+        .onReceive(timer, perform: { _ in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                    timeRemaining -= 1
+            } else {
+                
+            }
+        })
+        .onChange(of: scenePhase) { _, _ in
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
